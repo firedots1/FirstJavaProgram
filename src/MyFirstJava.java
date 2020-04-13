95cbdfab53b004abb59ac7bfe69de5776180ebd2
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-13 13:48:20 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,13 @@
+
+/*
+This is the first Java program which will print out a message, Hello World.
+ */
+
+public class MyFirstJava {
+    public static void main (String[] args)
+    {
+
+        System.out.println("Hello world.");
+
+    }
+}
